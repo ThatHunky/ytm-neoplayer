0ac1e4e3e0c54015170f0c150ee69e7e26d25a21
ThatHunky <thathunky29@gmail.com>
2025-06-07 16:53:32 +0300

    fix: handle pyinstaller entry

--- ytm_player/__main__.py
+++ ytm_player/__main__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import sys
 from PySide6.QtWidgets import QApplication  # type: ignore
 
-from .ui.main_window import MainWindow
+from ytm_player.ui.main_window import MainWindow
 
 
 def main() -> None:
